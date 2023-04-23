da09b0ea31409774afe96e3c0ee5f27fd7dbf342
Raj Gaurav <singhggaurav14@gmail.com>
2023-04-23 21:54:02 +0530

    saving my code

--- ray_tracing/subastian_ray_tracing.py
+++ ray_tracing/subastian_ray_tracing.py
@@ -1,6 +1,6 @@
 import pygame as pg
 import numpy as np
-from math import cos,sin
+from math import cos,sin,tan
 
 dist = lambda p,q:sum((p-q)**2)**0.5
 mag = lambda v:dist(v,np.zeros(v.size))
@@ -13,12 +13,29 @@ class MainCamera:
         self.resolution=np.array([x, y])
         self.side_x = int(pyg.width/self.resolution[0])
         self.side_y = int(pyg.height/self.resolution[1])
-        self.field_of_view = 1/2
+        self.field_of_view = 50
+        self.nearClipPlane=10
+        self.aspect_ratio=1
         self.a=0
         self.b=0
         self.x=normalize(np.array([cos(self.a),sin(self.a),0]))
         self.y=normalize(np.array([0,cos(self.b),sin(self.b)]))
         self.z=normalize(np.cross(self.x,self.y))
+    def render(self,pyg):
+        planeHeight=self.nearClipPlane*tan(self.field_of_view*0.5*(3.141596/180))*2
+        planeWidth =planeHeight*self.aspect_ratio
+
+        bottomLeftLocal = np.array([-planeWidth/2,-planeHeight/2,self.nearClipPlane])
+        for i in range(self.resolution[0]):
+            for j in range(self.resolution[1]):
+                tx= i/(self.resolution[0]-1)
+                ty= j/(self.resolution[1]-1)
+
+                pointLocal = bottomLeftLocal + np.array([planeWidth*tx,planeHeight*ty,0])
+                point = self.pos+self.x*pointLocal[0]+self.y*pointLocal[1]+self.z*pointLocal[2]
+                dir = normalize(point-self.pos)
+                col = np.array([np.dot(dir,np.array([1,0,0])),np.dot(dir,np.array([0,1,0])),np.dot(dir,np.array([0,0,1]))])
+                pg.draw.rect(pyg.window, pyg.clipArray(col*255,0,255),[i*self.side_x,j*self.side_y,self.side_x,self.side_y],0)
     def move(self,pyg):
         if "left" in pyg.buttons:self.a+=0.1
         elif "right" in pyg.buttons:self.a-=0.1
@@ -29,30 +46,30 @@ class MainCamera:
         self.x=normalize(np.array([cos(self.a),sin(self.a),0]))
         self.y=normalize(np.array([0,cos(self.b),sin(self.b)]))
         self.z=normalize(np.cross(self.x,self.y))
-    def render(self, pyg, objects):
-        for y in range(self.resolution[1]):
-            for x in range(self.resolution[0]):
-                col = self.shoot_ray(x, y, objects)
-                pg.draw.rect(pyg.window, pyg.clipArray(col*255,0,255),[x*self.side_x,y*self.side_y,self.side_x,self.side_y],0)
-    def shoot_ray(self,x,y, objects):
-        z=self.z*self.field_of_view
-        x=(self.x*(1/self.resolution[0])*(x-(self.resolution[0]/2)))
-        y=(self.y*(1/self.resolution[1])*(y-(self.resolution[1]/2)))
-        p0=x+y+z
-        vec=normalize(p0+self.pos)
-        col=self.intersection(vec,objects)
-        # return np.array([np.dot(vec,np.array([1,0,0])),np.dot(vec,np.array([0,1,0])),np.dot(vec,np.array([0,0,1]))])
-        return col
-    # def intersection(self,vec,objects):
-    #     for object in objects:
-    #         dit=(np.dot(self.pos,vec)**2)-(np.dot(self.pos,self.pos)-(object.radi*object.radi))
-    #         print(dit)
-    #         if dit<0:return np.zeros(3)
-    #         return np.ones(3)
-    #         # return (dit-np.dot(self.pos,vec))
-    def intersection(self,vec,objects):
-        for object in objects:
-            mag(self.pos+(vec*t)-(object.pos-self.pos))=object.radi
+    # def render(self, pyg, objects):
+    #     for y in range(self.resolution[1]):
+    #         for x in range(self.resolution[0]):
+    #             col = self.shoot_ray(x, y, objects)
+    #             pg.draw.rect(pyg.window, pyg.clipArray(col*255,0,255),[x*self.side_x,y*self.side_y,self.side_x,self.side_y],0)
+    # def shoot_ray(self,x,y, objects):
+    #     z=self.z*self.field_of_view
+    #     x=(self.x*(1/self.resolution[0])*(x-(self.resolution[0]/2)))
+    #     y=(self.y*(1/self.resolution[1])*(y-(self.resolution[1]/2)))
+    #     p0=x+y+z
+    #     vec=normalize(p0+self.pos)
+    #     col=self.intersection(vec,objects)
+    #     # return np.array([np.dot(vec,np.array([1,0,0])),np.dot(vec,np.array([0,1,0])),np.dot(vec,np.array([0,0,1]))])
+    #     return col
+    # # def intersection(self,vec,objects):
+    # #     for object in objects:
+    # #         dit=(np.dot(self.pos,vec)**2)-(np.dot(self.pos,self.pos)-(object.radi*object.radi))
+    # #         print(dit)
+    # #         if dit<0:return np.zeros(3)
+    # #         return np.ones(3)
+    # #         # return (dit-np.dot(self.pos,vec))
+    # # def intersection(self,vec,objects):
+    # #     for object in objects:
+    # #         mag(self.pos+(vec*t)-(object.pos-self.pos))=object.radi
                 
 class Object:
     def __init__(self,pos,col,is_light):
@@ -102,5 +119,5 @@ camera = MainCamera(pyg,60,60)
 objectsInScene = [Sphere([0,0,50],1,[1,0,0])]
 
 while pyg.update():
-    camera.render(pyg,objectsInScene)
+    camera.render(pyg)
     camera.move(pyg)
